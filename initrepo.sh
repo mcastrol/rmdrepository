f9da47c614d006b45a62bbed6497ddb10ac5e5aa
mcastrol <mcastrol@gmail.com>
2018-04-12 11:49:23 +0200

    branch gh-pages

---
+++ initrepo.sh
@@ -0,0 +1,7 @@
+echo "# rmdrepository" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/mcastrol/rmdrepository.git
+git push -u origin master
+
